df47a783e0ca210989b6775e10e37ef002a0a3ef
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-10-01 18:03:45 -0500

    Clase de encriptacion
    Se realiza la implementacion de la clase para realizar la encriptacion de la contraseña

--- WorldTools.API/Controllers/UserController.cs
+++ WorldTools.API/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Win32;
+using WorldTools.API.Utils.Encrypt;
 using WorldTools.Application.Gateway;
 using WorldTools.Domain.Commands.UserCommands;
 using WorldTools.Domain.ResponseVm.User;
@@ -21,6 +22,10 @@ namespace WorldTools.API.Controllers
         [HttpPost("register")]
         public async Task<UserResponseVm> RegisterUser([FromBody] RegisterUserCommand command)
         {
+            string salt = PasswordEncryption.GenerateSalt();
+            string hashedPassword = PasswordEncryption.EncryptPassword(command.UserPassword, salt);
+
+            command.UserPassword = hashedPassword;
             return await _userUseCase.RegisterUser(command);
         }
     }
